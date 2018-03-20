3711c95ba594bc0d9abb452ea476771b38d29831
thinkupp <thexwant@gmail.com>
2018-03-20 11:39:17 +0800

    修改一些细节

--- src/main.js
+++ src/main.js
@@ -2,4 +2,4 @@ import VTouch from './Vtouch'
 
 export default VTouch;
 
-if(typeof window !== undefined && window.Vue) window.Vue.component('v-touch', VTouch);
+if(typeof window !== undefined && window.Vue) window.Vue.component('upp-touch', VTouch);
